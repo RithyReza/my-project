e62126ad4a1a8a380e0057c769a30ef79e128946
thy <thyreza69gmail@gmail.com>
2025-11-26 11:58:26 +0700

    add

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,6 @@
 import { io } from "socket.io-client";
 
-const API_URL =
-  import.meta.env.VITE_API_URL ||
-  "http://localhost:5000";
-
-export const socket = io(API_URL, {
+export const socket = io(import.meta.env.VITE_API_URL, {
   transports: ["websocket"],
+  secure: true,
 });
